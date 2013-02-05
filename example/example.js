8aa1f91bc1cd16609b274e9dd9929889564a630c
Callum Stott <oetzi101@gmail.com>
2013-02-05 00:17:45 -0800

    Decouple writing and reporting and start implementation of nested writing

--- example/example.js
+++ example/example.js
@@ -3,13 +3,13 @@ function exampleTest() {
     setup: function(test) {
       return test();
     },
-  
+
     "can do addition": function() {
       return (5 + 4) == 9;
     },
-  
+
     "is from 1984": function() {
       return (2 + 2) == 5;
     }
-  });  
+  });
 }
--- leica.js
+++ leica.js
@@ -1,40 +1,74 @@
-window.leica = {  
-  runner: {
-    writeSuite: function(description) {
-      var featureHeader = document.createElement('h2');
-      featureHeader.setAttribute('class', 'feature');
-      featureHeader.innerHTML = description + ":";
-      document.getElementById("results").appendChild(featureHeader);
+window.leica = {
+    runner: {
+        success: 0,
+        failure: 0,
+        history: [],
+
+        depth: function() {
+          return this.history.length - 1;
+        },
+
+        startSuite: function(description) {
+            this.history.push({ success: 0, failure: 0 });
+            window.leica.writer.writeSuite(description, this.depth());
+        },
+
+        finishSuite: function() {
+            this.history.pop();
+        },
+
+        reportFeature: function(feature, result) {
+            if (result) {
+                this.history[this.depth()].success++;
+                this.success++;
+            } else {
+                this.history[this.depth()].failure++;
+                this.failure++;
+            }
+
+            window.leica.writer.writeFeature(feature, result, this.depth());
+        }
     },
 
-    writeFeature: function(description, result) { 
-      var descriptionText = description + "... ";
-      var resultText = result ? "Passed" : "Failed";
-      var resultClass = result ? "pass" : "fail";
-                
-      var resultElement = document.createElement('p');
-      resultElement.setAttribute('class', 'result ' + resultClass);
-      resultElement.innerHTML = descriptionText + resultText;
-      document.getElementById("results").appendChild(resultElement);
+    writer: {
+        writeSuite: function(description, depth) {
+            var featureHeader = document.createElement('h2');
+            featureHeader.setAttribute('class', 'feature');
+            featureHeader.innerHTML = description + ":";
+            document.getElementById("results").appendChild(featureHeader);
+        },
+
+        writeFeature: function(feature, result, depth) {
+            var descriptionText = feature + "... ";
+            var resultText = result ? "Passed" : "Failed";
+            var resultClass = result ? "pass" : "fail";
+
+            var resultElement = document.createElement('p');
+            resultElement.setAttribute('class', 'result ' + resultClass);
+            resultElement.innerHTML = descriptionText + resultText;
+            document.getElementById("results").appendChild(resultElement);
+        }
     },
-    
+
     tests: function(func) {
-      window.onload = function() {
-        func();
-      }
+        window.onload = function() {
+            func();
+        }
     }
-  }
 };
 
+
 window.describe = function(description, testObject) {
-  window.leica.runner.writeSuite(description);
+    window.leica.runner.startSuite(description);
 
-  for (var feature in testObject) {
-    var test = testObject[feature];
+    for (var feature in testObject) {
+        var test = testObject[feature];
 
-    if (test != testObject.setup) {
-      var result = testObject.setup(test);
-      window.leica.runner.writeFeature(feature, result);
+        if (test != testObject.setup) {
+            var result = testObject.setup(test);
+            window.leica.runner.reportFeature(feature, result)
+        }
     }
-  }
+
+    window.leica.runner.finishSuite();
 };
